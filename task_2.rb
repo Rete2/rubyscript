7472bb692e53d6d5f42ad3b03ec9dce8c2ff38eb
Rete2 <rete@posteo.de>
2016-11-09 19:35:51 +0100

    Task 2 solved

---
+++ task_2.rb
@@ -0,0 +1,15 @@
+puts "\"I want a dyke for president"
+puts "..."
+puts "I want a Black woman for president"
+puts "I want someone with bad teeth...\""
+puts "Zoe Leonard"
+puts "1992\v"
+
+poem = "\"I want a dyke for president\n
+...\n
+I want a Black woman for president\n
+I want someone with bad teeth...\"\n
+Zoe Leonard\n
+1992\n"
+
+puts poem
